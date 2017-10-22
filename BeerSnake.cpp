d09253c9a491f530de8072d3e8675e6444c8a786
Filip Leski <filtyc@gmail.com>
2017-10-22 15:37:59 -0400

    Implement the logic behind snake's movement

--- BeerSnake.cpp
+++ BeerSnake.cpp
@@ -16,10 +16,12 @@ void BeerSnake::loadImages() {
 
 void BeerSnake::startGame() {
    length = 2;
+   direction = BeerSnake::UP;
    snakeXs.at(0) = (ITEMS_HORIZONTALLY/2-1)*ITEM_SIDE;
    snakeYs.at(0) = (ITEMS_VERTICALLY/2-1)*ITEM_SIDE;
    snakeXs.at(1) = snakeXs.at(0);
    snakeYs.at(1) = snakeYs.at(0) + ITEM_SIDE;
+   startTimer(STEP_TIME);
 }
 
 void BeerSnake::paintEvent(QPaintEvent *e) {
@@ -32,4 +34,23 @@ void BeerSnake::paintEvent(QPaintEvent *e) {
    for (int piece = 1; piece < length; piece++) {
       painter.drawImage(snakeXs.at(piece), snakeYs.at(piece), body);
    }
+}
+
+void BeerSnake::move() {
+   for (int piece = (length - 1); piece > 0; piece--) {
+      snakeXs.at(piece) = snakeXs.at(piece - 1);
+      snakeYs.at(piece) = snakeYs.at(piece - 1);
+   }
+
+   switch(direction) {
+      case BeerSnake::UP: {
+         snakeYs.at(0) -= ITEM_SIDE;
+      }
+   }
+}
+
+void BeerSnake::timerEvent(QTimerEvent *e) {
+   Q_UNUSED(e);
+   move();
+   repaint();
 }
--- BeerSnake.h
+++ BeerSnake.h
@@ -11,6 +11,7 @@ public:
    BeerSnake(QWidget *parent = 0);
 protected:
    void paintEvent(QPaintEvent *);
+   void timerEvent(QTimerEvent *);
 private:
    static const int ITEM_SIDE = 30;
    static const int ITEMS_HORIZONTALLY = 20;
@@ -18,10 +19,13 @@ private:
    static const int BOARD_WIDTH = ITEM_SIDE * ITEMS_HORIZONTALLY;
    static const int BOARD_HIGHT = ITEM_SIDE * ITEMS_VERTICALLY;
    static const int MAX_LENGTH = ITEMS_HORIZONTALLY * ITEMS_VERTICALLY;
+   enum Direction {UP, RIGHT, DOWN, LEFT};
+   static const int STEP_TIME = 200;
 
    int length;
    std::array<int, MAX_LENGTH> snakeXs;
    std::array<int, MAX_LENGTH> snakeYs;
+   int direction;
 
    QImage head;
    QImage body;
@@ -29,5 +33,6 @@ private:
 
    void loadImages();
    void startGame();
+   void move();
 
 };
