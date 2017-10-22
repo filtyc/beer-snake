42dfbdf632579ac2d62a179a1874ce55ac30d3f4
Filip Leski <filtyc@gmail.com>
2017-10-22 15:18:21 -0400

    Draw a static version of game's initial screen

--- BeerSnake.cpp
+++ BeerSnake.cpp
@@ -1,5 +1,35 @@
 #include <BeerSnake.h>
+#include <QPainter>
 
 BeerSnake::BeerSnake(QWidget *parent) : QWidget(parent) {
-   resize(600,600);
+   setStyleSheet("background-color:white;");
+   resize(BOARD_WIDTH, BOARD_HIGHT);
+   loadImages();
+   startGame();
+}
+
+void BeerSnake::loadImages() {
+   head.load("images/head.png");
+   body.load("images/body.png");
+   beer.load("images/beer.png");
+}
+
+void BeerSnake::startGame() {
+   length = 2;
+   snakeXs.at(0) = (ITEMS_HORIZONTALLY/2-1)*ITEM_SIDE;
+   snakeYs.at(0) = (ITEMS_VERTICALLY/2-1)*ITEM_SIDE;
+   snakeXs.at(1) = snakeXs.at(0);
+   snakeYs.at(1) = snakeYs.at(0) + ITEM_SIDE;
+}
+
+void BeerSnake::paintEvent(QPaintEvent *e) {
+   Q_UNUSED(e);
+
+   QPainter painter(this);
+
+   painter.drawImage(snakeXs.at(0), snakeYs.at(0), head);
+
+   for (int piece = 1; piece < length; piece++) {
+      painter.drawImage(snakeXs.at(piece), snakeYs.at(piece), body);
+   }
 }
--- BeerSnake.h
+++ BeerSnake.h
@@ -1,10 +1,33 @@
 #pragma once
 
 #include <QWidget>
+#include <QImage>
+#include <array>
+
 
 class BeerSnake : public QWidget {
 
 public:
    BeerSnake(QWidget *parent = 0);
+protected:
+   void paintEvent(QPaintEvent *);
+private:
+   static const int ITEM_SIDE = 30;
+   static const int ITEMS_HORIZONTALLY = 20;
+   static const int ITEMS_VERTICALLY = 20;
+   static const int BOARD_WIDTH = ITEM_SIDE * ITEMS_HORIZONTALLY;
+   static const int BOARD_HIGHT = ITEM_SIDE * ITEMS_VERTICALLY;
+   static const int MAX_LENGTH = ITEMS_HORIZONTALLY * ITEMS_VERTICALLY;
+
+   int length;
+   std::array<int, MAX_LENGTH> snakeXs;
+   std::array<int, MAX_LENGTH> snakeYs;
+
+   QImage head;
+   QImage body;
+   QImage beer;
+
+   void loadImages();
+   void startGame();
 
 };
